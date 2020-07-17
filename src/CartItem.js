4dc9c7c93e8aebefd4ce5159112967fa42ba918b
Avinash Kumar <avinash999yadav@gmail.com>
2020-07-17 18:56:18 +0530

    modify state

--- src/CartItem.js
+++ src/CartItem.js
@@ -2,7 +2,40 @@ import React from 'react';
 
 class CartItem  extends React.Component {
 
+constructor(){
+
+super();
+this.state = {
+  price: 222,
+  title:'Mobile',
+  qty: 10,
+  img: ''
+
+}
+
+}
+
+ increaseQuantity = () => {
+
+// this.setState({
+//   qty : this.state.qty + 1
+// });
+
+ this.setState((prevState) => {
+
+   return {
+
+      qty : prevState.qty + 1
+
+   }
+
+ });
+ }
+
+
 render(){
+
+  const {price,title,qty} = this.state;
 
 return(
 
@@ -11,11 +44,27 @@ return(
    <img style = {styles.img}/>
  </div>
  <div className="right-block">
-   <div style = {{color:'#777'}}>Phone</div>  
-   <div>Rs 999</div>  
-   <div>Qty: 1</div>  
+   <div style = {{fontSize:25}}>{title}</div>  
+   <div style = {{color:'#777'}}>Rs {price}</div>  
+<div style = {{color:'#777'}}>Qty:{qty}</div>  
    <div className="cart-item-actions">
      {/* Buttons */ }
+     <img 
+     alt="increase" 
+     className="action-icons" 
+     src="https://image.flaticon.com/icons/svg/992/992651.svg"
+     onClick = {this.increaseQuantity}
+     />
+     <img 
+     alt="decrease" 
+     className="action-icons" 
+     src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
+     />
+     <img 
+     alt="delete" 
+     className="action-icons" 
+     src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+     />
     </div>  
  </div>
 </div>
@@ -28,8 +77,8 @@ return(
 const styles = {
 
   img :{
-    height:110,
-    width:110,
+    height:130,
+    width:130,
     borderRadius:4,
     background : '#777'
   }
