eea0d8ee88c747951b9fc331bb448f841e2e3304
Avinash Kumar <avinash999yadav@gmail.com>
2020-07-18 00:13:42 +0530

    Complete Card Project

--- src/Cart.js
+++ src/Cart.js
@@ -1,121 +1,23 @@
 import React from 'react';
-import CardItem from './CartItem'
+import CartItem from './CartItem';
 
-class Cart  extends React.Component {
-
-    constructor(){
-
-        super();
-        this.state = {
-            products : [
-
-               {
-                   price: 10,
-                   title:'Mobile',
-                   qty: 1,
-                   img: '',
-                   id :1
-               }, 
-               {
-                  price: 20,
-                  title:'Watch',
-                  qty: 2,
-                  img: '',
-                  id :2
-               }, 
-               {
-                  price: 30,
-                  title:'Laptop',
-                  qty: 3,
-                  img: '',
-                  id :3
-               },   
-            
-            
-            ]
-        
-        }
-        
-        }
-
-     handleIncreaseQuantity = (product) => {
-
-    const {products} = this.state;
-    const index = products.indexOf(product);
-
-    products[index].qty +=1;
-
-    this.setState({products})
-
-
-     }   
-
-     handleDecreaseQuantity = (product) => {
-
-        const {products} = this.state;
-        const index = products.indexOf(product);
-    
-        if(products[index].qty == 0){
-
-                 return;            
-        }
-        products[index].qty -=1;
-    
-        this.setState({products})
-    
-    
-         }  
-
-
-         handleDeleteProduct = (id) =>{
-
-         const {products} = this.state;
-
-         const items = products.filter((item) => item.id != id);
-
-         this.setState({
-
-          products:items
-
-         });
-
-
-
-
-         }
-render(){
-
-    const {products} = this.state
-
-   return(
-
+const Cart = (props) => {
+  const { products } = props;
+  return (
     <div className="cart">
-
-     {products.map((product) => {
-
-        return(
-
-            <CardItem 
-            product = {product}
-            key = {product.id}
-            onIncreaseQuantity = {this.handleIncreaseQuantity}
-            onDecreaseQuantity = {this.handleDecreaseQuantity}
-            onDeleteProduct = {this.handleDeleteProduct}
-            />
-
-        );
-
-     })}
-   
-
+      {products.map((product) => {
+        return (
+          <CartItem
+            product={product}
+            key={product.id}
+            onIncreaseQuantity={props.onIncreaseQuantity}
+            onDecreaseQuantity={props.onDecreaseQuantity}
+            onDeleteProduct={props.onDeleteProduct}
+          />
+        )
+      })}
     </div>
-
-   );
-
+  );
 }
-
-
-}
-
 
 export default Cart;
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,111 +1,55 @@
 import React from 'react';
 
-class CartItem  extends React.Component {
-
-constructor(){
-
-super();
-this.state = {
-  price: 222,
-  title:'Mobile',
-  qty: 1,
-  img: ''
-
-}
-
-}
-
- increaseQuantity = () => {
-
-// this.setState({
-//   qty : this.state.qty + 1
-// });
-
- this.setState((prevState) => {
-
-   return {
-
-      qty : prevState.qty + 1
-
-   }
-
- });
- }
- 
- decreaseQuantity = () => {
-
-  const {qty} = this.state;
-
-  if(qty == 0 ){
-    return;
-  }
-  
-   this.setState((prevState) => {
-  
-     return {
-  
-        qty : prevState.qty - 1
-  
-     }
-  
-   });
-   }
-
-
-render(){
-
-  const {price,title,qty} = this.props.product;
-  const {product,onIncreaseQuantity,onDecreaseQuantity,onDeleteProduct} = this.props;
-
-return(
-
-<div className="cart-item">
- <div className="left-block">
-   <img style = {styles.img}/>
- </div>
- <div className="right-block">
-   <div style = {{fontSize:25}}>{title}</div>  
-   <div style = {{color:'#777'}}>Rs {price}</div>  
-<div style = {{color:'#777'}}>Qty:{qty}</div>  
-   <div className="cart-item-actions">
-     {/* Buttons */ }
-     <img 
-     alt="increase" 
-     className="action-icons" 
-     src="https://image.flaticon.com/icons/svg/992/992651.svg"
-     onClick = {() => onIncreaseQuantity(product)}
-     />
-     <img 
-     alt="decrease" 
-     className="action-icons" 
-     src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
-     onClick = {() => onDecreaseQuantity(product)}
-     />
-     <img 
-     alt="delete" 
-     className="action-icons" 
-     src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
-     onClick = {() => onDeleteProduct(product.id)}
-     />
-    </div>  
- </div>
-</div>
-);
-
-}
-
+const CartItem = (props) => {
+  const { price, title, qty } = props.product;
+  const {
+    product,
+    onIncreaseQuantity,
+    onDecreaseQuantity,
+    onDeleteProduct
+  } = props;
+  return (
+    <div className="cart-item">
+      <div className="left-block">
+        <img style={styles.image} src={product.img} />
+      </div>
+      <div className="right-block">
+        <div style={ { fontSize: 25 } }>{title}</div>
+        <div style={ { color: '#777' } }>Rs {price} </div>
+        <div style={ { color: '#777' } }>Qty: {qty} </div>
+        <div className="cart-item-actions">
+          {/* Buttons */}
+          <img
+            alt="increase"
+            className="action-icons"
+            src="https://image.flaticon.com/icons/svg/992/992651.svg"
+            onClick={() => onIncreaseQuantity(product)}
+          />
+          <img
+            alt="decrease"
+            className="action-icons"
+            src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
+            onClick={() => onDecreaseQuantity(product)}
+          />
+          <img
+            alt="delete"
+            className="action-icons"
+            src="https://image.flaticon.com/icons/svg/1214/1214428.svg"
+            onClick={() => onDeleteProduct(product.id)}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 const styles = {
-
-  img :{
-    height:130,
-    width:130,
-    borderRadius:4,
-    background : '#777'
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: '#ccc'
   }
-
-
 }
 
 export default CartItem;
